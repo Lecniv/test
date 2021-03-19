28b5043bdac2019d0b3a67dbc4884748a0a87a24
Lecniv <paywitowt.bel@gmail.com>
2021-03-19 23:18:38 +0300

    new

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <conio.h>
 #include <stdio.h>
+#include <locale.h>
 
 int main()
 {
+    system("chcp 1251");
+    setlocale(LC_CTYPE, "rus");
     printf("hello world!");
     getch();
+    printf("Да здравствуют славяне!!!");
+    getch();
+    return 0;
+}
 }
